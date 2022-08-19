5aaf7f3b9b6b3aedd87a253cba7ae699b08b6d17
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-08-19 16:02:22 -0300

    carrousel basics added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import '../styles/Footer.css';
-import Logo from '../images/logo-2.png'
 
 function Footer() {
     return (
         <div className="Footer-container">
             <footer>
-                <img src={Logo}></img>
+                <img src="images/logo-2.png"></img>
                 <div className="Footer-nav" >
                     <a>Terms and Conditions</a>
                     <a>Contact Us</a>
