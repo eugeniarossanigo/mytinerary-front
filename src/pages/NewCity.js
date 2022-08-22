5390bd8bfb9a56c21c87c561d532998836c09ad9
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-08-22 18:46:33 -0300

    Created NewCity

---
+++ src/pages/NewCity.js
@@ -0,0 +1,11 @@
+import CitiesLayout from '../layouts/CitiesLayout'
+
+function NewCity() {
+    return (
+        <>
+            <CitiesLayout />
+        </>
+    );
+}
+
+export default NewCity;
