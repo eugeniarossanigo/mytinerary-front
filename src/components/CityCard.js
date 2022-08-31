179370bce5870735994be2326b7bdb8905b4cbaa
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-08-31 14:15:57 -0300

    link cities to details

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -1,17 +1,20 @@
 import '../styles/CityCard.css'
+import {Link as LinkRouter} from 'react-router-dom'
+
 
 function CityCard(props) {
     var city = props.data
     console.log(props)
     return (
         <>
-            <a class="card" href="#">
+            <LinkRouter className="card" to={`city?id=${city._id}`}>
                 <div className="card__background" style={{ backgroundImage: `url(${city.url})`}} ></div>
                 <div className="card__content">
                     <p className="card__category">City</p>
                     <h3 className="card__heading">{city.title}</h3>
+                 T  
                 </div>
-            </a>
+                </LinkRouter>
         </>
     )
 }
