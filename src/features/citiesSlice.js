6e3299f75f7072dab1a3fda30623999414ea888b
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-02 15:33:03 -0300

    Redux

---
+++ src/features/citiesSlice.js
@@ -0,0 +1,21 @@
+import {createSlide} from '@reduxjs/toolkit'
+import axios from 'axios'
+
+export const citiesSlice = createSlice({
+    name : 'cities',
+
+    initialState : {
+        cities : []
+    },
+
+    reducers : {
+        fetchFromServer : (state) => {
+            state.cities = [{
+                    
+            }]
+        }
+    }
+})
+export const {fetchFromServer} = citiesSlice.actions
+
+export default citiesSlice.reducer
