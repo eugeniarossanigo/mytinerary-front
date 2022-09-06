2a9011ea6c88f771176b10cb5701d28674199167
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-06 18:44:54 -0300

    itineraryCard Component

---
+++ src/components/ActivitiyCard.js
@@ -0,0 +1,16 @@
+import '../styles/ActivityCard.css';
+
+
+function ActivityCard() {
+    }
+    return (
+    <>
+            <div className='activity-card'>
+                <h3>Cordoba</h3>
+                <img src="./carloz-paz-5308013.jpg" alt=""/>
+            </div>
+    </>
+    );
+    
+
+export default ActivityCard;
---
+++ src/components/ItineraryCard.js
@@ -0,0 +1,40 @@
+import '../styles/ItineraryCard.css';
+
+function ItineraryCard() {
+}
+return (
+    <>
+        <div className='itinerary-container'>
+            <div>
+                <h1>Pure Adenaline</h1>
+            </div>
+            <div className='itinerary-info'>
+                <h3>Duration:</h3>
+                <h3>Price:</h3>
+            </div>
+            <div>
+                <div class="activities-container">
+
+                </div>
+                <h2>Leave us your comment</h2>
+            </div>
+
+            <div className='comment-container'>
+                <div className='comment'>
+                    <div className='pers'>
+                        <h4>Euge</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                    <div className='pers'>
+                        <h4>Marcos</h4>
+                        <p>Lorem ipsum dolor sit.</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </>
+);
+
+
+export default ItineraryCard;
