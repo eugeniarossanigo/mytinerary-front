9d9c8935c7edcfc34667e3b1efb953c831ddd69c
eugeniarossanigo <eugeniarossanigo@gmail.com>
2022-09-13 16:34:29 -0300

    signup with redux

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,6 +2,8 @@ import React from "react";
 import SignUpGoogle from '../components/SignUpGoogle'
 import InputUsers from "../components/InputUsers";
 import '../styles/SignUp.css'
+import { useGetNewUserMutation } from "../features/usersAPI";
+import { useRef } from "react";
 
 const inputsArray = [
     { _id: 600, name: "name", type: "text" },
@@ -9,27 +11,29 @@ const inputsArray = [
     { _id: 602, name: "mail", type: "email" },
     { _id: 603, name: "password", type: "text" },
     { _id: 604, name: "photo", type: "text" },
-    { _id: 605, name: "country", type: "text" },
-    { _id: 606, name: "role", type: "text" }
+    { _id: 605, name: "country", type: "text" }
 ]
 
-
 export default function SignUp() {
+    
+    const newInputs = useRef({})
+    let data = {from: 'form', role: 'user'}
 
-    // const handleSignin = (e) => {
-    //     e.preventDefault()
-    //     const formSignin = document.getElementById('Form-signin')
-    //     const formObject = Object.fromEntries(new FormData(newInputs.current))
-    //     console.log(formObject)
-    //     setNewData(formObject)
-    //     formSignIn.reset()
-    // }
+    const [addUser] = useGetNewUserMutation();
+
+    const handleSignup = async(e) => {
+        e.preventDefault()
+        const formSignup = document.getElementById('Form-signup')
+        const formObject = Object.fromEntries(new FormData(newInputs.current))
+        let newUserData = {...formObject, ...data}
+        await addUser(newUserData)
+        formSignup.reset()
+    }
 
     return (
         <>
             <main>
-                <form id="Form-signup">
-                    {/* onSubmit={HandleSignin} */}
+                <form id="Form-signup" onSubmit={handleSignup} ref={newInputs}>
                     <h2>CREATE YOUR ACCOUNT</h2>
                     {inputsArray.map(inputObj => <InputUsers inputObj={inputObj} values={""} />)}
                     <div className="button-container">
