c635f37820b916f6d132967fba3b0dd211f3bd09
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-13 13:46:03 -0300

    component for SignUp page Created

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,11 +1,42 @@
 import React from "react";
 import SignUpGoogle from '../components/SignUpGoogle'
+import InputUsers from "../components/InputUsers";
+import '../styles/SignUp.css'
+
+const inputsArray = [
+    { _id: 600, name: "name", type: "text" },
+    { _id: 601, name: "lastName", type: "text" },
+    { _id: 602, name: "mail", type: "email" },
+    { _id: 603, name: "password", type: "text" },
+    { _id: 604, name: "photo", type: "text" },
+    { _id: 605, name: "country", type: "text" },
+    { _id: 606, name: "role", type: "text" }
+]
+
 
 export default function SignUp() {
+
+    // const handleSignin = (e) => {
+    //     e.preventDefault()
+    //     const formSignin = document.getElementById('Form-signin')
+    //     const formObject = Object.fromEntries(new FormData(newInputs.current))
+    //     console.log(formObject)
+    //     setNewData(formObject)
+    //     formSignIn.reset()
+    // }
+
     return (
         <>
             <main>
-                <SignUpGoogle /> 
+                <form id="Form-signup">
+                    {/* onSubmit={HandleSignin} */}
+                    <h2>CREATE YOUR ACCOUNT</h2>
+                    {inputsArray.map(inputObj => <InputUsers inputObj={inputObj} values={""} />)}
+                    <div className="button-container">
+                        <button className="Form-btn" type="submit">SIGN UP</button>
+                        <SignUpGoogle />
+                    </div>
+                </form>
             </main>
         </>
     );
