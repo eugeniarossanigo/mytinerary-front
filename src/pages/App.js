8ce8a2c739335ac47495ee64752d2f9f39e3ba7a
AmuchasteguiMarcos317 <amuchasteguimarcos317@gmail.com>
2022-09-14 18:08:53 -0300

    component for page MyTineraries Created

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 import '../styles/App.css';
 import Home from '../pages/Home';
-import {BrowserRouter, Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cities from './Cities';
 import NewCity from './NewCity';
 import UnderConstruction from './UnderConstruction';
@@ -13,26 +13,26 @@ import SignUp from './SignUp';
 import SignIn from './SignIn';
 
 function App() {
-    return (
+  return (
     <>
-        <BrowserRouter>
-          <ScrollToTop />
-          <CitiesLayout>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/auth/signup' element={<SignUp />} />
-              <Route path='/auth/signin' element={<SignIn />} />
-              <Route path='/cities' element={<Cities />} />
-              <Route path='/cities/:id' element={<Details />} />
-              <Route path='/newcity' element={<NewCity />} />
-              <Route path='/editcity' element={<EditCity />} />
-              <Route path='/mytinerary/:id' element={<MyTineraries />} />
-              <Route path='*' element={<UnderConstruction />} />
-            </Routes>
-          </CitiesLayout>
-        </BrowserRouter>
+      <BrowserRouter>
+        <ScrollToTop />
+        <CitiesLayout>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/auth/signup' element={<SignUp />} />
+            <Route path='/auth/signin' element={<SignIn />} />
+            <Route path='/cities' element={<Cities />} />
+            <Route path='/cities/:id' element={<Details />} />
+            <Route path='/newcity' element={<NewCity />} />
+            <Route path='/editcity' element={<EditCity />} />
+            <Route path='/mytinerary/:id' element={<MyTineraries />} />
+            <Route path='*' element={<UnderConstruction />} />
+          </Routes>
+        </CitiesLayout>
+      </BrowserRouter>
     </>
-    );
+  );
 }
 
 export default App;
